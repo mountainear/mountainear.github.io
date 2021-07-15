7734000217068ec38b612a3e7c7f11050837a9d1
Anna Siebenbrunner <72545186+anna7br@users.noreply.github.com>
2021-07-15 21:27:45 +0200

    delete watermark double

--- map.js
+++ map.js
@@ -80,24 +80,6 @@ let layerControl = L.control.layers({
 }).addTo(map);
 overlays.at.addTo(map);
 
-// add Watermark
-L.Control.Watermark = L.Control.extend({
-    onAdd: function (map) {
-        var img = L.DomUtil.create('img');
-        img.src = 'icons/mountainear/mountainear_lang.png';
-        img.style.width = '100px';
-        return img;
-    },
-    onRemove: function (map) {
-        // Nothing to do here
-    }
-});
-L.control.watermark = function (opts) {
-    return new L.Control.Watermark(opts);
-}
-L.control.watermark({
-    position: 'topright'
-}).addTo(map);
 
 // adjust zoom level and center to selected featuregroup layer
 map.on('layeradd layerremove', function () {
