ffd850500261bf5c27fc0e5eb45547488c65865f
Anna Siebenbrunner <72545186+anna7br@users.noreply.github.com>
2021-07-15 20:23:57 +0200

    delete old comments

--- map.js
+++ map.js
@@ -80,29 +80,6 @@ let layerControl = L.control.layers({
 }).addTo(map);
 overlays.at.addTo(map);
 
-// Karte initialisieren und auf Österreichs Mittelpunkt blicken
-/*
-let map = L.map("map", {
-    fullscreenControl: true,
-    center: [47.71216, 13.34290],
-    zoom: 7,
-    layers: [
-        L.tileLayer('https://api.mapbox.com/styles/v1/hanna8br/ckr4r0ztm0pzu17mpgr24nkln/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGFubmE4YnIiLCJhIjoiY2tpYWNudXViMGhscDJ5cmtxOTliZDl0NSJ9.KIYizFkjQ7ODuRfxwFBEtg', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            tileSize: 512,
-            zoomOffset: -1,
-        })
-        //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-    ]
-});
-// summer URL
-'https://api.mapbox.com/styles/v1/hanna8br/ckr3mc3gnes5h18mkuzxtvxwn/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGFubmE4YnIiLCJhIjoiY2tpYWNudXViMGhscDJ5cmtxOTliZDl0NSJ9.KIYizFkjQ7ODuRfxwFBEtg'
-// winter URL
-'https://api.mapbox.com/styles/v1/hanna8br/ckr4r0ztm0pzu17mpgr24nkln/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGFubmE4YnIiLCJhIjoiY2tpYWNudXViMGhscDJ5cmtxOTliZDl0NSJ9.KIYizFkjQ7ODuRfxwFBEtg'
-*/
-
-
 // add Watermark
 L.Control.Watermark = L.Control.extend({
     onAdd: function (map) {
